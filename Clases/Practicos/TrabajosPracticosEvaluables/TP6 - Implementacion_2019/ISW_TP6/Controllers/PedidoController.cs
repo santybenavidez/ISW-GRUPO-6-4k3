eca4df08bd4bf827c60b60177b64ba855f4fc3b4
santybenavidez <42209168+santybenavidez@users.noreply.github.com>
2019-10-24 18:06:12 -0300

    TP KANBAN
    Inclusion archivos tp kanban

---
+++ Clases/Practicos/TrabajosPracticosEvaluables/TP6 - Implementacion_2019/ISW_TP6/Controllers/PedidoController.cs
@@ -0,0 +1,48 @@
+﻿using ISW_TP6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISW_TP6.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET: Pedido
+        public ActionResult Pedido()
+        {
+            PedidoModel Modelo;
+
+            try
+            {
+                Modelo = new PedidoModel();
+                Modelo.Ciudades = DBMock.DBMock.GetAllCiudades();
+                Modelo.Horas = DBMock.DBMock.GetAllHorarios();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.MensajeError = ex.Message;
+                return View("Error");
+            }
+
+            return View(Modelo);
+        }
+
+        [HttpPost]
+        public ActionResult Crear(PedidoModel pedidoModel)
+        {
+            try
+            {
+                DBMock.DBMock.RegistrarPedido(pedidoModel);
+            }
+            catch(Exception ex)
+            {
+                ViewBag.MensajeError = ex.Message;
+                return View("Error");
+            }
+
+            return View("Exito");
+        }
+    }
+}
